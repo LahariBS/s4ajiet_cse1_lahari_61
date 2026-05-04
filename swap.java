ac1e7c8005f415d412841d71de159b734123076c
Lahari B Suvarna <laharibsuvarna@gmail.com>
2026-05-04 11:37:14 +0530

    Implement swap functionality in swap.java

---
+++ swap.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+        int a=1,b=3;
+        int c;
+        c =a;
+        a=b;
+        b=c;
+        System.out.println("a=" +a+",b="+b);
+        
+    }
+}
