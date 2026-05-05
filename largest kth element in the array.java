15b0df1a0e9be6cf22408a33d205949d26906ea5
Lahari B Suvarna <laharibsuvarna@gmail.com>
2026-05-05 10:21:27 +0530

    k-th largest element finder in an array
    Implement a program to find the k-th largest element in an array.

---
+++ largest kth element in the array.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.Arrays;
+class Main {
+    public static void main(String[] args) {
+        int [] arr={40,10,0,100,80};
+        int i;
+        int large=arr[0];
+        int n=arr.length;
+          int k = 2;
+          Arrays.sort(arr);
+        for(i=0;i<n;i++){
+        if (arr[i]<large){
+            large=arr[i];
+        }
+            
+        
+        }
+          System.out.println("largest : " + large);
+        System.out.println("elements in the array  : " +n);
+         System.out.println(arr[n - k]);
+    }
+}
