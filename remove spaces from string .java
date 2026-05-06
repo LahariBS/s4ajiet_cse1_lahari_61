05fc163c1ab7619950c9231cc6a55b924ce85e2f
Lahari B Suvarna <laharibsuvarna@gmail.com>
2026-05-06 15:33:13 +0530

    Update remove spaces from string .java

--- remove spaces from string .java
+++ remove spaces from string .java
@@ -1,9 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        String str = "Hello   World\tJava";
-        
-        String result = str.replaceAll("\\s", "");
-        
-        System.out.println(result);
+
+        String input = "hello world  javaa";
+        StringBuilder k = new StringBuilder();
+
+        for (char j : input.toCharArray()) {
+            if (!Character.isWhitespace(j)) {
+                k.append(j);
+            }
+        }
+
+        String m = k.toString();
+        System.out.println(m);
     }
 }
