cdbcbd0b314c147f6a8dce40b1dd5c449d6df168
Lahari B Suvarna <laharibsuvarna@gmail.com>
2026-05-06 11:18:35 +0530

    Add LinkedList implementation in Java
    Implemented a Java program using LinkedList to demonstrate basic operations such as adding, removing, and retrieving elements.

---
+++ LinkedList in java.java
@@ -0,0 +1,27 @@
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        LinkedList<Integer> cse = new LinkedList<>();
+
+        cse.add(1);
+        cse.add(7);
+        cse.add(80);
+        cse.add(90);
+        cse.add(78);
+        cse.addFirst(5);
+        cse.addLast(6);
+        
+        
+        cse.removeFirst();
+         cse.removeLast();
+         cse.remove(2);
+         
+         cse.getFirst();
+         cse.getLast();
+        System.out.println(cse.get(0));
+       
+        System.out.println(cse);
+    }
+}
