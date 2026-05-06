87184f3405b5e7249f10596762a86861f7948ecf
Lahari B Suvarna <laharibsuvarna@gmail.com>
2026-05-06 15:12:24 +0530

    Create anagram 1.java

---
+++ anagram 1.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class Main {
+
+    
+    public static boolean isAnagram(String s1, String s2) {
+
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+
+        char[] s11 = s1.toCharArray();
+        char[] s22 = s2.toCharArray();
+
+        Arrays.sort(s11);
+        Arrays.sort(s22);
+
+        return Arrays.equals(s11, s22);
+    }
+
+    public static void main(String[] args) {
+
+        String s1 = "listen";
+        String s2 = "silent";
+
+        System.out.println(isAnagram(s1, s2));
+    }
+}
